742ae27bb5e9be0845e3c1ce3022976ffac628ba
root <root@wubblesmemories.com>
2012-02-25 19:50:57 +0000

    some minor bug fixes with submitting rsvps

--- src/Wedding/RSVP/Edit.php
+++ src/Wedding/RSVP/Edit.php
@@ -3,12 +3,15 @@ class Wedding_RSVP_Edit extends Wedding_RSVP_Record
 {
     function handlePost($post = array()) {
         if (!isset($post['name']) || empty($post['name'])) {
-            throw new Exception("RSVP has not been saved!  No name was provided", 400);
+            throw new \Exception("RSVP has not been saved!  No name was provided", 400);
         }
         
         $this->synchronizeWithArray($post);
-        $this->save();
-        
-        \Epoch\Controller::redirect(Wedding_Controller::$url . "success");
+       
+       if(!$result = $this->save()) {
+         throw new \Excaption("Could not save!", 500);
+       }
+       
+       \Epoch\Controller::redirect(Wedding_Controller::$url . "success");
     }
 }
