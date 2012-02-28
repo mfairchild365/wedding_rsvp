48c828ce5bbb8a43ba17d36638968073384b452c
mfairchild365 <mfairchild365@gmail.com>
2012-02-27 23:06:40 -0600

    Add our picture to the homepage.

--- www/templates/default/Wedding/Home/View.tpl.php
+++ www/templates/default/Wedding/Home/View.tpl.php
@@ -5,4 +5,6 @@
   <ul>
     <li><a href="<?php Wedding_Controller::$url?>RSVP">RSVP</a></li>
   </ul>
+  
+  <img src="<?php Wedding_Controller::$url?>www/images/amymike.jpg" width="700" height="394"/>
 </div>
